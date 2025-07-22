ca13083b4fa8ffe2610d303bd50aa2ebac4b0b1b
Keonta <keontapeat@gmail.com>
2025-07-22 16:44:20 -0400

    Initial Commit

---
+++ Planet ProTrader (Clean)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planet ProTrader (Clean)
+//
+//  Created by Keonta  on 7/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planet ProTrader (Clean)/Planet_ProTrader__Clean_App.swift
@@ -0,0 +1,17 @@
+//
+//  Planet_ProTrader__Clean_App.swift
+//  Planet ProTrader (Clean)
+//
+//  Created by Keonta  on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Planet_ProTrader__Clean_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
